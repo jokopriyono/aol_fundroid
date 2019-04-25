208e86f3e8a5eb0b0aa891b47ca77a645827af09
Joko Priyono <joe.mandrake007@gmail.com>
2019-04-26 01:47:37 +0700

    Memunculkan loading view

--- FormTastic/app/src/main/java/com/jokopriyono/form/MainActivity.kt
+++ FormTastic/app/src/main/java/com/jokopriyono/form/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.jokopriyono.form
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.text.Html
 import android.util.Patterns
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = getString(R.string.agreement)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            txt_agreement.text = Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        else
+            txt_agreement.text = Html.fromHtml(text)
 
         checkbox.setOnCheckedChangeListener { _, isChecked -> btn_register.isEnabled = isChecked }
         btn_register.setOnClickListener { validate() }
@@ -42,6 +57,24 @@ class MainActivity : AppCompatActivity() {
     private fun g(ids: Int): String = getString(ids)
 
     private fun process() {
+        relative_loading.visibility = VISIBLE
+        stateClickable(false)
+        handler.postDelayed({
+            runOnUiThread {
+                relative_loading.visibility = GONE
+                stateClickable(true)
+                alert(getString(R.string.success)) { yesButton { getString(R.string.ok) } }.show()
+            }
+        }, 2000)
+    }
 
+    private fun stateClickable(b: Boolean) {
+        edt_first_name.isEnabled = b
+        edt_last_name.isEnabled = b
+        edt_password.isEnabled = b
+        edt_confirm_password.isEnabled = b
+        edt_email.isEnabled = b
+        checkbox.isClickable = b
+        btn_register.isClickable = b
     }
 }
